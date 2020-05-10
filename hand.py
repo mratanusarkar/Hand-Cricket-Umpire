6854cb35b9d09efee6fe0ae45369496b221e7744
Atanu Sarkar <mratanusarkar@gmail.com>
2020-05-11 01:34:36 +0530

    added getHSV to get hsv threshold

--- hand.py
+++ hand.py
@@ -1,9 +1,11 @@
 import numpy as np
 import math
 import cv2
+from getHSV import get_hsv
 
 # video capture
 cap = cv2.VideoCapture(0)
+lower_bound, upper_bound = get_hsv(cap)
 
 while cap.isOpened():
 
@@ -18,8 +20,8 @@ while cap.isOpened():
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # define range of hsv skin color
-    lower_skin = np.array([0, 0, 40], dtype=np.uint8)
-    upper_skin = np.array([150, 80, 160], dtype=np.uint8)
+    lower_skin = np.array(lower_bound, dtype=np.uint8)
+    upper_skin = np.array(upper_bound, dtype=np.uint8)
 
     # extract image portion that has skin color (hand)
     img_skin = cv2.inRange(img_hsv, lower_skin, upper_skin)
@@ -56,66 +58,66 @@ while cap.isOpened():
 
     # count the number of defects in the convex hull
     defect_count = 0
-    for defect in defects:
-        p1, p2, p3, d = defect[0]
-        first_point = tuple(approx[p1][0])
-        last_point = tuple(approx[p2][0])
-        in_point = tuple(approx[p3][0])
+    try:
+        for defect in defects:
+            p1, p2, p3, d = defect[0]
+            first_point = tuple(approx[p1][0])
+            last_point = tuple(approx[p2][0])
+            in_point = tuple(approx[p3][0])
 
-        # find the length of all sides of the triangles
-        a = math.sqrt((last_point[0] - first_point[0])**2 + (last_point[1] - first_point[1])**2)
-        b = math.sqrt((in_point[0] - first_point[0]) ** 2 + (in_point[1] - first_point[1]) ** 2)
-        c = math.sqrt((last_point[0] - in_point[0]) ** 2 + (last_point[1] - in_point[1]) ** 2)
+            # find the length of all sides of the triangles
+            a = math.sqrt((last_point[0] - first_point[0])**2 + (last_point[1] - first_point[1])**2)
+            b = math.sqrt((in_point[0] - first_point[0]) ** 2 + (in_point[1] - first_point[1]) ** 2)
+            c = math.sqrt((last_point[0] - in_point[0]) ** 2 + (last_point[1] - in_point[1]) ** 2)
 
-        # using Heron's Formula to find the area
-        s = (a + b + c) / 2
-        triangle_area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+            # using Heron's Formula to find the area
+            s = (a + b + c) / 2
+            triangle_area = math.sqrt(s*(s-a)*(s-b)*(s-c))
 
-        # distance between the point and the convex hull
-        distance = (2 * triangle_area) / a
+            # distance between the point and the convex hull
+            distance = (2 * triangle_area) / a
 
-        # finding out the angle using cosine rule := c^2 = a^2 + b^2 − 2ab cos(C)
-        angle = math.acos((b**2 + c**2 - a**2) / (2 * b * c))   # in radian
-        angle = angle * 180 / math.pi                           # in degree
+            # finding out the angle using cosine rule := c^2 = a^2 + b^2 − 2ab cos(C)
+            angle = math.acos((b**2 + c**2 - a**2) / (2 * b * c))   # in radian
+            angle = angle * 180 / math.pi                           # in degree
 
-        if angle <= 90 and distance > 30:
-            defect_count += 1
-            cv2.circle(img, in_point, 3, [255, 0, 0], -1)
+            if angle <= 90 and distance > 30:
+                defect_count += 1
+                cv2.circle(img, in_point, 3, [255, 0, 0], -1)
 
-        # draw lines connecting the fingers
-        cv2.line(img, first_point, last_point, [0, 255, 0], 2)
+            # draw lines connecting the fingers
+            cv2.line(img, first_point, last_point, [0, 255, 0], 2)
 
-        # number of defects = no of fingers-1 in this method.
+            # number of defects = no of fingers-1 in this method.
 
-        # calculate the number of fingers and display the output
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        line = cv2.LINE_AA
-        if defect_count == 0:
-            if area_contour < 2000:
-                cv2.putText(img, "No Hand Detected", (0, 50), font, 2, (0, 0, 255), 3, line)
-            else:
-                # zero
-                if area_ratio < 12:
-                    cv2.putText(img, "0", (0, 50), font, 2, (0, 0, 255), 3, line)
-                elif area_ratio < 17.5:
-                    cv2.putText(img, "6", (0, 50), font, 2, (0, 0, 255), 3, line)
+            # calculate the number of fingers and display the output
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            line = cv2.LINE_AA
+            if defect_count == 0:
+                if area_contour < 2000:
+                    cv2.putText(img, "No Hand Detected", (0, 50), font, 2, (0, 0, 255), 3, line)
                 else:
-                    cv2.putText(img, "1", (0, 50), font, 2, (0, 0, 255), 3, line)
-        elif defect_count == 1:
-            cv2.putText(img, "2", (0, 50), font, 2, (0, 0, 255), 3, line)
-        elif defect_count == 2:
-            cv2.putText(img, "3", (0, 50), font, 2, (0, 0, 255), 3, line)
-        elif defect_count == 3:
-            cv2.putText(img, "4", (0, 50), font, 2, (0, 0, 255), 3, line)
-        elif defect_count == 4:
-            cv2.putText(img, "5", (0, 50), font, 2, (0, 0, 255), 3, line)
-        else:
-            cv2.putText(img, "Reposition Your Hand", (0, 50), font, 2, (0, 0, 255), 3, line)
+                    # zero
+                    if area_ratio < 12:
+                        cv2.putText(img, "0", (0, 50), font, 2, (0, 0, 255), 3, line)
+                    elif area_ratio < 17.5:
+                        cv2.putText(img, "6", (0, 50), font, 2, (0, 0, 255), 3, line)
+                    else:
+                        cv2.putText(img, "1", (0, 50), font, 2, (0, 0, 255), 3, line)
+            elif defect_count == 1:
+                cv2.putText(img, "2", (0, 50), font, 2, (0, 0, 255), 3, line)
+            elif defect_count == 2:
+                cv2.putText(img, "3", (0, 50), font, 2, (0, 0, 255), 3, line)
+            elif defect_count == 3:
+                cv2.putText(img, "4", (0, 50), font, 2, (0, 0, 255), 3, line)
+            elif defect_count == 4:
+                cv2.putText(img, "5", (0, 50), font, 2, (0, 0, 255), 3, line)
+            else:
+                cv2.putText(img, "Reposition Your Hand", (0, 50), font, 2, (0, 0, 255), 3, line)
+    except:
+        print("No hand and defects fount! error in processing the image!")
 
-    # display the frame
     cv2.imshow('frame', img)
-
-    # quit
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
